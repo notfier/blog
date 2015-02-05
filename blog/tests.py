177f331908db39269ab36f0d262047787a296bb1
notfier <notfier@gmail.com>
2015-02-05 15:48:18 +0200

    removed unnecessary test-cases

--- blog/tests.py
+++ blog/tests.py
@@ -1,66 +1,11 @@
 from django.test import  TestCase, Client
 from django.core.urlresolvers import reverse
 
-from test_blog.settings import BASE_DIR, MEDIA_URL
 from blog.models import Post, Tag, Comments
 
 import os
 
 # Create your tests here.
-
-
-class PostTest(TestCase):
-    """
-    Check the post creation.
-    """
-
-    def setUp(self):
-        c = Client()
-        with open(os.path.join(BASE_DIR, 'media/tests/trueorfalse.png'),
-                                                            'rb') as fp:
-            Post.objects.create(title="TestPost",
-                                text="I am testing robot, yeah, yeah.",
-                                slug="TestPost")
-            c.post(MEDIA_URL, {'picture': fp})
-
-    def test_result(self):
-        test_post = Post.objects.get(title="TestPost")
-        self.assertEqual(test_post.title, 'TestPost')
-
-
-class TestTag(TestCase):
-    """
-    Check the tag adding.
-    """
-
-    def setUp(self):
-        c = Client()
-        Tag.objects.create(name="test tag", slug="test_slug")
-
-    def test_result(self):
-        test_tag = Tag.objects.get(name='test tag')
-        self.assertEqual(test_tag.name, 'test tag')
-
-
-class TestComments(TestCase):
-    """
-    Check the comments creation.
-    """
-
-    def setUp(self):
-        c = Client()
-        Post.objects.create(title="TestPost", text="I am tester.",
-                            slug="TestPost")
-        Comments.objects.create(comment=Post.objects.get(
-                                title="TestPost"),
-                                author="Tester",
-                                comment_text="I am testero.")
-
-    def test_result(self):
-        comment = Comments.objects.get(comment=Post.objects.get(
-                                       title="TestPost"),
-                                       comment_text="I am testero.")
-        self.assertEqual(comment.comment_text, 'I am testero.')
 
 
 class TestViews(TestCase):
